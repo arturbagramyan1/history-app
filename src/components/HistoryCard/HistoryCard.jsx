f4732c0148183eeb6b32cabe95e88b434cdc57f2
Artur Bagramyan <arthurbaghramian2003@gmail.com>
2025-04-20 19:57:08 +0400

    feat: add HistoryCard component Display

--- src/components/HistoryCard/HistoryCard.jsx
+++ src/components/HistoryCard/HistoryCard.jsx
@@ -1,5 +1,32 @@
 function HistoryCard({ item }) {
-	return <div>{item.id}</div>;
+	const eventDate = new Date(item.event_date_utc).toLocaleDateString();
+
+	return (
+		<div className="">
+			<h2>{item.title}</h2>
+			<p>
+				<strong>Date:</strong> {eventDate}
+			</p>
+			<p>{item.details}</p>
+			<div className="">
+				{item.links.article && (
+					<a href={item.links.article} target="_blank">
+						Article
+					</a>
+				)}
+				{item.links.wikipedia && (
+					<a href={item.links.wikipedia} target="_blank">
+						Wikipedia
+					</a>
+				)}
+				{item.links.reddit && (
+					<a href={item.links.reddit} target="_blank" rel="noopener noreferrer">
+						Reddit
+					</a>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default HistoryCard;
